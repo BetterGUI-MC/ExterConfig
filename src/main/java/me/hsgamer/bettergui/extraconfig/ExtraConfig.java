f988ba51cbcb372c7eb1f4c06e6a4c5f85b3c316
HSGamer <huynhqtienvtag@gmail.com>
2023-03-03 19:50:45 +0700

    add yaml

--- src/main/java/me/hsgamer/bettergui/extraconfig/ExtraConfig.java
+++ src/main/java/me/hsgamer/bettergui/extraconfig/ExtraConfig.java
@@ -4,14 +4,17 @@ import me.hsgamer.bettergui.builder.ConfigBuilder;
 import me.hsgamer.hscore.bukkit.addon.PluginAddon;
 import me.hsgamer.hscore.config.simplixstorage.JsonProvider;
 import me.hsgamer.hscore.config.simplixstorage.TomlProvider;
+import me.hsgamer.hscore.config.simplixstorage.YamlProvider;
 
 public final class ExtraConfig extends PluginAddon {
     private final TomlProvider tomlProvider = new TomlProvider();
     private final JsonProvider jsonProvider = new JsonProvider();
+    private final YamlProvider yamlProvider = new YamlProvider();
 
     @Override
     public void onEnable() {
         ConfigBuilder.INSTANCE.register(tomlProvider::loadConfiguration, "toml");
         ConfigBuilder.INSTANCE.register(jsonProvider::loadConfiguration, "json");
+        ConfigBuilder.INSTANCE.register(yamlProvider::loadConfiguration, "yaml", "yml");
     }
 }
